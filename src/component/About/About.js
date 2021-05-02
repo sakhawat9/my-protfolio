f0580f9e8a39031e6de2e5b6b6d409a16a0dcd8f
SakhawatHossain55 <sakhawatHossain7969@gmail.com>
2021-05-02 21:21:56 +0600

    added about button

--- src/component/About/About.js
+++ src/component/About/About.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
+import { useHistory } from 'react-router';
 import image from '../../images/my picture 1.png'
 import './About.css'
 
@@ -13,6 +14,8 @@ const About = () => {
                             <img style={{width : '350px'}} className="pb-5" src={image} alt=""/>
                             <h1>I'm Sakhawat Hossain</h1>
                             <p>A self-motivated and enthusiastic web developer with a deep interest in JavaScript. To work in the Software industry with modern web technologies of different local & multinational Software/ IT agencies of Bangladesh and grow rapidly with increasing responsibilities.</p>
+                            <button className="btn-github btn-brand"><a target="_blank" href="https://github.com/SakhawatHossain55">Github</a></button>
+                            <button className="btn-linkedIn btn-brand"><a target="_blank" href="https://www.linkedin.com/in/sakhawat-hossain-95a57b203">LinkedIn</a></button>
                         </div>
                     </div>
                     <div className="col-md-6 p-5">
