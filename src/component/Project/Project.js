a5afd5b5778892cf28373194ca1f39a1ba3b53e5
sakhawat9 <sakhawathossain7969@gmail.com>
2022-03-25 10:52:00 +0600

    [ADD] projects

--- src/component/Project/Project.js
+++ src/component/Project/Project.js
@@ -88,22 +88,113 @@ const data = [
     liveLink: "https://ecommercestore-alpha.vercel.app",
   },
   {
-    title: "Fstore",
+    title: "Nilam",
     descriptor:
-      "Fstore is a professional dynamic e-commerce web application where users can choose and explore product details. Users can select items and can add them to the cart section. Finally can order them from the checkout section.",
-    image: [`${fashion}`, `${fashion2}`, `${fashion3}`],
+      "Nilam is a professional and minimal-looking domain selling template. Nilam is made with working contact forms with Google SMTP. It comes with six unique layouts which include tons of sections, customization options, and features for multiple purposes.      ",
+    image: [`${fstore}`, `${fstore2}`, `${fstore3}`],
     technology: [
       "React.js",
-      "React-redux",
+      "Next.js",
       "Typescript",
       "React icon",
-      "React bootstrap",
-      "sass",
-      "eslint",
+      "Tallwind CSS",
+      "Nodemailer",
+      "react-toastify",
+      "axios",
       "vercel",
     ],
-    github: "https://github.com/sakhawat9/fstore",
-    liveLink: "https://ecommercestore-alpha.vercel.app",
+    github: "https://github.com/foysalimran/nilam",
+    liveLink: "https://nilam-demo.vercel.app",
+  },
+  {
+    title: "Chart App",
+    descriptor:
+      "This is a Communicating website where a person can contact him/her friend & customer community. If a person wants to use this website, he/she must first sign in to the Google Authentication process. They can create Room and contact easily.",
+    image: [`${chart_app01}`, `${chart_app02}`, `${chart_app03}`],
+    technology: [
+      "React.js",
+      "React Redux",
+      "Redux Toolkit",
+      "Material-ui",
+      "Style Component",
+      "Firebase",
+      "React Firebase Hook",
+    ],
+    github: "https://github.com/sakhawat9/chat-app",
+    liveLink: "https://chat-app-team-78.web.app",
+  },
+  {
+    title: "Fast Repair",
+    descriptor:
+      "A full-stack single-page web application where customers can book a service for repairing their smart phone by pay the fees. The website contains a separate dashboard for admin and user.",
+    image: [`${fast_repair01}`, `${fast_repair02}`, `${fast_repair03}`],
+    technology: [
+      "React.js",
+      "Node.js",
+      "Express.js",
+      "MOngoDB",
+      "React bootstrap",
+      "Material-ui",
+      "Stripe.js",
+      "FontAwesome",
+      "Firebase",
+    ],
+    github: "https://github.com/sakhawat9/fast-repair",
+    liveLink: "https://fast-repair-c13c2.web.app",
+  },
+  {
+    title: "Daily Mart",
+    descriptor:
+      "A full-stack single-page e-commerce web application where users can choose, explore and order food by submitting relevant information.",
+    image: [`${daily_mart01}`, `${daily_mart02}`, `${daily_mart03}`],
+    technology: [
+      "React.js",
+      "Node.js",
+      "Express.js",
+      "MOngoDB",
+      "React bootstrap",
+      "Material-ui",
+      "FontAwesome",
+      "Firebase",
+    ],
+    github: "https://github.com/sakhawat9/Daily-Mart",
+    liveLink: "https://fresh-valley-e1a56.web.app",
+  },
+  {
+    title: "Doctors Portal",
+    descriptor:
+      "A single-page web app with a dashboard where people can pick the date and time and fix an appointment. A doctor can review his appointments and prescribe medicine to a Patient from his dashboard.",
+    image: [`${doctors_portal01}`, `${doctors_portal02}`, `${doctors_portal03}`],
+    technology: [
+      "React.js",
+      "Node.js",
+      "Express.js",
+      "MOngoDB",
+      "React bootstrap",
+      "Material-ui",
+      "FontAwesome",
+      "Firebase",
+    ],
+    github: "https://github.com/sakhawat9/doctors-portal-main",
+    liveLink: "https://doctors-portal-ac941.web.app",
+  },
+  {
+    title: "volunteer_networks",
+    descriptor:
+    "A single-page web app. Admin can add any event and he can see all events. Users can register for an event and see all registered event details. with a dashboard where people can pick the date and time and fix an appointment.",
+    image: [`${volunteer01}`, `${volunteer02}`, `${volunteer03}`],
+    technology: [
+      "React.js",
+      "Node.js",
+      "Express.js",
+      "MOngoDB",
+      "React bootstrap",
+      "Material-ui",
+      "FontAwesome",
+      "Firebase",
+    ],
+    github: "https://github.com/sakhawat9/volunteer-network",
+    liveLink: "https://volunteer-network-3bcf3.web.app",
   },
 ];
 
@@ -171,384 +262,7 @@ const Project = () => {
             </div>
           ))}
           {/* 
-          <div className="mb-4 col-lg-4 col-sm-6">
-            <div className="p-3 text-justify rounded shadow cart-style img-animation">
-              <Carousel activeIndex={index} onSelect={handleSelect}>
-                <Carousel.Item>
-                  <img
-                    style={{ height: "150px" }}
-                    className="d-block w-100"
-                    src={fashion}
-                    alt="First slide"
-                  />
-                </Carousel.Item>
-                <Carousel.Item>
-                  <img
-                    style={{ height: "150px" }}
-                    className="d-block w-100"
-                    src={fashion2}
-                    alt="Second slide"
-                  />
-                </Carousel.Item>
-                <Carousel.Item>
-                  <img
-                    style={{ height: "150px" }}
-                    className="d-block w-100"
-                    src={fashion3}
-                    alt="Third slide"
-                  />
-                </Carousel.Item>
-              </Carousel>
-              <h3>Fstore</h3>
-              <p>
-                Fstore is a professional dynamic e-commerce web application
-                where users can choose and explore product details. Users can
-                select items and can add them to the cart section. Finally can
-                order them from the checkout section.
-              </p>
-              <div className="px-3 pb-2 row technology">
-                <span>React.js</span>
-                <span>React-redux</span>
-                <span>Typescript</span>
-                <span>React icon</span>
-                <span>React bootstrap</span>
-                <span>sass</span>
-                <span>eslint</span>
-                <span>vercel</span>
-              </div>
-              <a
-                style={{ fontSize: "30px", paddingRight: "20px" }}
-                target="_blank"
-                rel="noreferrer"
-                href="https://github.com/sakhawat9/ecommerce_store"
-              >
-                <FontAwesomeIcon icon={faGithub} />
-              </a>
-              <a
-                style={{ fontSize: "30px" }}
-                target="_blank"
-                rel="noreferrer"
-                href="https://ecommercestore-alpha.vercel.app"
-              >
-                <FontAwesomeIcon icon={faExternalLinkAlt} />
-              </a>
-            </div>
-          </div>
-          <div className="mb-4 col-lg-4 col-sm-6">
-            <div className="p-3 text-justify rounded shadow cart-style img-animation">
-              <Carousel activeIndex={index} onSelect={handleSelect}>
-                <Carousel.Item>
-                  <img
-                    style={{ height: "150px" }}
-                    className="d-block w-100"
-                    src={fstore}
-                    alt="First slide"
-                  />
-                </Carousel.Item>
-                <Carousel.Item>
-                  <img
-                    style={{ height: "150px" }}
-                    className="d-block w-100"
-                    src={fstore2}
-                    alt="Second slide"
-                  />
-                </Carousel.Item>
-                <Carousel.Item>
-                  <img
-                    style={{ height: "150px" }}
-                    className="d-block w-100"
-                    src={fstore3}
-                    alt="Third slide"
-                  />
-                </Carousel.Item>
-              </Carousel>
-              <h3>Nilam</h3>
-              <p>
-                Nilam is a professional and minimal-looking domain selling
-                template. Nilam is made with working contact forms with Google
-                SMTP. It comes with six unique layouts which include tons of
-                sections, customization options, and features for multiple
-                purposes.
-              </p>
-              <div className="px-3 pb-2 row technology">
-                <span>React.js</span>
-                <span>Next.js</span>
-                <span>Typescript</span>
-                <span>React icon</span>
-                <span>Tallwind CSS</span>
-                <span>Nodemailer</span>
-                <span>react-toastify</span>
-                <span>axios</span>
-                <span>vercel</span>
-              </div>
-              <a
-                style={{ fontSize: "30px", paddingRight: "20px" }}
-                target="_blank"
-                rel="noreferrer"
-                href="https://github.com/foysalimran/nilam"
-              >
-                <FontAwesomeIcon icon={faGithub} />
-              </a>
-              <a
-                style={{ fontSize: "30px" }}
-                target="_blank"
-                rel="noreferrer"
-                href="https://nilam-demo.vercel.app"
-              >
-                <FontAwesomeIcon icon={faExternalLinkAlt} />
-              </a>
-            </div>
-          </div>
-          <div className="mb-4 col-lg-4 col-sm-6">
-            <div className="p-3 text-justify rounded shadow cart-style img-animation">
-              <Carousel activeIndex={index} onSelect={handleSelect}>
-                <Carousel.Item>
-                  <img
-                    style={{ height: "150px" }}
-                    className="d-block w-100"
-                    src={chart_app01}
-                    alt="First slide"
-                  />
-                </Carousel.Item>
-                <Carousel.Item>
-                  <img
-                    style={{ height: "150px" }}
-                    className="d-block w-100"
-                    src={chart_app02}
-                    alt="Second slide"
-                  />
-                </Carousel.Item>
-                <Carousel.Item>
-                  <img
-                    style={{ height: "150px" }}
-                    className="d-block w-100"
-                    src={chart_app03}
-                    alt="Third slide"
-                  />
-                </Carousel.Item>
-              </Carousel>
-              <h3>Chart App</h3>
-              <p>
-                This is a Communicating website where a person can contact
-                him/her friend & customer community. If a person wants to use
-                this website, he/she must first sign in to the Google
-                Authentication process. They can create Room and contact easily.
-              </p>
-              <div className="px-3 pb-2 row technology">
-                <span>React Redux</span>
-                <span>Redux Toolkit</span>
-                <span>Style Component</span>
-                <span>Material-ui</span>
-                <span>Firebase</span>
-                <span>React Firebase Hook</span>
-              </div>
-              <div>
-                <a
-                  style={{ fontSize: "30px", paddingRight: "20px" }}
-                  target="_blank"
-                  rel="noreferrer"
-                  href="https://github.com/sakhawat9/chat-app"
-                >
-                  <FontAwesomeIcon icon={faGithub} />
-                </a>
-                <a
-                  style={{ fontSize: "30px" }}
-                  target="_blank"
-                  rel="noreferrer"
-                  href="https://chat-app-team-78.web.app"
-                >
-                  <FontAwesomeIcon icon={faExternalLinkAlt} />
-                </a>
-              </div>
-            </div>
-          </div>
-          <div className="mb-4 col-lg-4 col-sm-6">
-            <div className="p-3 text-justify rounded shadow cart-style img-animation ">
-              <Carousel activeIndex={index} onSelect={handleSelect}>
-                <Carousel.Item>
-                  <img
-                    style={{ height: "150px" }}
-                    className="d-block w-100"
-                    src={fast_repair01}
-                    alt="First slide"
-                  />
-                </Carousel.Item>
-                <Carousel.Item>
-                  <img
-                    style={{ height: "150px" }}
-                    className="d-block w-100"
-                    src={fast_repair02}
-                    alt="Second slide"
-                  />
-                </Carousel.Item>
-                <Carousel.Item>
-                  <img
-                    style={{ height: "150px" }}
-                    className="d-block w-100"
-                    src={fast_repair03}
-                    alt="Third slide"
-                  />
-                </Carousel.Item>
-              </Carousel>
-              <h3>Fast Repair</h3>
-              <p>
-                A full-stack single-page web application where customers can
-                book a service for repairing their smart phone by pay the fees.
-                The website contains a separate dashboard for admin and user.
-              </p>
-              <div className="px-3 pb-2 row technology">
-                <span>React.js</span>
-                <span>Node.js</span>
-                <span>Express.js</span>
-                <span>MOngoDB</span>
-                <span>Material-ui</span>
-                <span>Stripe.js</span>
-                <span>FontAwesome</span>
-                <span>Firebase</span>
-                <span>React bootstrap</span>
-              </div>
-              <a
-                style={{ fontSize: "30px", paddingRight: "20px" }}
-                target="_blank"
-                rel="noreferrer"
-                href="https://github.com/sakhawat9/fast-repair"
-              >
-                <FontAwesomeIcon icon={faGithub} />
-              </a>
-              <a
-                style={{ fontSize: "30px" }}
-                target="_blank"
-                rel="noreferrer"
-                href="https://fast-repair-c13c2.web.app"
-              >
-                <FontAwesomeIcon icon={faExternalLinkAlt} />
-              </a>
-            </div>
-          </div>
-          <div className="mb-4 col-lg-4 col-sm-6">
-            <div className="p-3 text-justify rounded shadow cart-style img-animation">
-              <Carousel activeIndex={index} onSelect={handleSelect}>
-                <Carousel.Item>
-                  <img
-                    style={{ height: "150px" }}
-                    className="d-block w-100"
-                    src={daily_mart01}
-                    alt="First slide"
-                  />
-                </Carousel.Item>
-                <Carousel.Item>
-                  <img
-                    style={{ height: "150px" }}
-                    className="d-block w-100"
-                    src={daily_mart02}
-                    alt="Second slide"
-                  />
-                </Carousel.Item>
-                <Carousel.Item>
-                  <img
-                    style={{ height: "150px" }}
-                    className="d-block w-100"
-                    src={daily_mart03}
-                    alt="Third slide"
-                  />
-                </Carousel.Item>
-              </Carousel>
-              <h3>Daily Mart</h3>
-              <p>
-                A full-stack single-page web application where users can choose,
-                explore and order food by submitting relevant information.
-              </p>
-              <div className="px-3 pb-2 row technology">
-                <span>React.js</span>
-                <span>Node.js</span>
-                <span>Express.js</span>
-                <span>MOngoDB</span>
-                <span>Material-ui</span>
-                <span>FontAwesome</span>
-                <span>Firebase</span>
-                <span>React bootstrap</span>
-              </div>
-              <a
-                style={{ fontSize: "30px", paddingRight: "20px" }}
-                target="_blank"
-                rel="noreferrer"
-                href="https://github.com/sakhawat9/Daily-Mart"
-              >
-                <FontAwesomeIcon icon={faGithub} />
-              </a>
-              <a
-                style={{ fontSize: "30px" }}
-                target="_blank"
-                rel="noreferrer"
-                href="https://fresh-valley-e1a56.web.app"
-              >
-                <FontAwesomeIcon icon={faExternalLinkAlt} />
-              </a>
-            </div>
-          </div>
-          <div className="mb-4 col-lg-4 col-sm-6">
-            <div className="p-3 text-justify rounded shadow cart-style img-animation">
-              <Carousel activeIndex={index} onSelect={handleSelect}>
-                <Carousel.Item>
-                  <img
-                    style={{ height: "150px" }}
-                    className="d-block w-100"
-                    src={doctors_portal01}
-                    alt="First slide"
-                  />
-                </Carousel.Item>
-                <Carousel.Item>
-                  <img
-                    style={{ height: "150px" }}
-                    className="d-block w-100"
-                    src={doctors_portal02}
-                    alt="Second slide"
-                  />
-                </Carousel.Item>
-                <Carousel.Item>
-                  <img
-                    style={{ height: "150px" }}
-                    className="d-block w-100"
-                    src={doctors_portal03}
-                    alt="Third slide"
-                  />
-                </Carousel.Item>
-              </Carousel>
-              <h3>Doctors Portal</h3>
-              <p>
-                A single-page web app with a dashboard where people can pick the
-                date and time and fix an appointment. A doctor can review his
-                appointments and prescribe medicine to a Patient from his
-                dashboard.
-              </p>
-              <div className="px-3 pb-2 row technology">
-                <span>React.js</span>
-                <span>Node.js</span>
-                <span>Express.js</span>
-                <span>MOngoDB</span>
-                <span>Material-ui</span>
-                <span>FontAwesome</span>
-                <span>Firebase</span>
-                <span>React bootstrap</span>
-              </div>
-              <a
-                style={{ fontSize: "30px", paddingRight: "20px" }}
-                target="_blank"
-                rel="noreferrer"
-                href="https://github.com/sakhawat9/doctors-portal-main"
-              >
-                <FontAwesomeIcon icon={faGithub} />
-              </a>
-              <a
-                style={{ fontSize: "30px" }}
-                target="_blank"
-                rel="noreferrer"
-                href="https://doctors-portal-ac941.web.app"
-              >
-                <FontAwesomeIcon icon={faExternalLinkAlt} />
-              </a>
-            </div>
-          </div>
+          
           <div className="mb-4 col-lg-4 col-sm-6">
             <div className="p-3 text-justify rounded shadow cart-style img-animation">
               <Carousel activeIndex={index} onSelect={handleSelect}>
